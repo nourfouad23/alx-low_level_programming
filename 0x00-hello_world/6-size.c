4cbd81a307047ba168bb7f05bb47816497b94087
nourfouad23 <n.fouad@xtoniq.com>
2023-04-20 16:40:05 -0700

    c files prints sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of a char: " + sizeof(char) + " byte(s)\n");
+printf("Size of a int: " + sizeof(int) + " byte(s)\n");
+printf("Size of a long int: " + sizeof(long int) + " byte(s)\n");
+printf("Size of a long long int: " + sizeof(long long int) + " byte(s)\n");
+printf("Size of a float: " + sizeof(float) + " byte(s)\n");
+return (0);
+}
