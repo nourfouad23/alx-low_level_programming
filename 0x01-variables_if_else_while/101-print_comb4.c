7291c5ef283770136948cc93da56683d06caff81
nourfouad23 <n.fouad@xtoniq.com>
2023-04-22 11:49:30 -0700

    3 digits comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+int z;
+
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 1 ; j < 10 ; j++)
+{
+for (z = 1 ; z < 10 ; z++)
+{
+if (i != j && j > i)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + z);
+if (j <= 8 && z <= 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
