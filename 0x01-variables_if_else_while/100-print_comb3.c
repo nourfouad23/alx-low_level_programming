49cb795d52e18de9ee1f6cc9a198199393514697
nourfouad23 <n.fouad@xtoniq.com>
2023-04-22 11:39:31 -0700

    fixes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,11 @@ for (i = 0 ; i < 10 ; i++)
 {
 for (j = 1 ; j < 10 ; j++)
 {
-if (i != j)
+if (i != j && j > i)
 {
 putchar('0' + i);
 putchar('0' + j);
-if (i <= 9 && j <= 9)
+if (i < 9 && j < 9)
 {
 putchar(',');
 putchar(' ');
