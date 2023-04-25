9778feece3f7a74e9f7ff744c6f1f1079966e0fa
nourfouad23 <n.fouad@xtoniq.com>
2023-04-25 15:17:18 -0700

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet ten times from a to z
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+char letter;
+
+for (i = 0; i <= 10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+}
+}
