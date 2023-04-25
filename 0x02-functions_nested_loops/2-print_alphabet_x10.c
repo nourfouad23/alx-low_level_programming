097f2fe8ea261155916af975a7e3b907f6a28a1f
nourfouad23 <n.fouad@xtoniq.com>
2023-04-25 15:22:11 -0700

    fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,13 @@ void print_alphabet_x10(void)
 int i;
 char letter;
 
-for (i = 0; i <= 10; i++)
+while (i <= 10)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 putchar(letter);
 }
-}
 putchar('\n');
+i++;
+}
 }
