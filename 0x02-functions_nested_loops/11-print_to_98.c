0c231e8377baf79831a4b4bc6687453d72acde0c
nourfouad23 <n.fouad@xtoniq.com>
2023-04-26 16:04:11 -0700

    function to print numbers till 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - function to check, print and return
+ *
+ * @n: Number to pass to function
+ *
+ */
+
+
+void print_to_98(int n)
+{
+int i;
+
+for (i = n; i <= 98; i ++)
+{
+if (i != 98)
+{
+_putchar('0' + i);
+_putchar(', ');
+}
+else
+{
+_putchar('0' + i);
+}
+}
+_putchar('\n');
+}
