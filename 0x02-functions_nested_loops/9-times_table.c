0aae9afc8512be8d8c470263c25060d8ce8b285e
nourfouad23 <n.fouad@xtoniq.com>
2023-04-26 16:27:52 -0700

    fixes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,14 +17,15 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 result = i * j;
-if (i == 9){
-printf("%d$\n", result);
-}
-else
+if (i != 9)
 {
 printf("%d, ", result);
 }
+else
+{
+printf("%d", result);
 }
 }
-printf("\n");
+printf("$\n");
+}
 }
