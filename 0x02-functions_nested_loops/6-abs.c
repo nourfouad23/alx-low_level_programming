fb48b5a3e7b9e73356e577813896dc4c2fa3e5c5
nourfouad23 <n.fouad@xtoniq.com>
2023-04-26 07:59:11 -0700

    abs function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - function to check, print and return
+ *
+ * @n: Number to pass to function
+ *
+ * Return: returns 1 or 0 or -1
+ */
+
+int _abs(int n)
+{
+return (n * (-1));
+}
