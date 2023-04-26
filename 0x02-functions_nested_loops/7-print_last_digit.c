bf7563e1b551b22cf4ef27ba2aa39def7a8836c0
nourfouad23 <n.fouad@xtoniq.com>
2023-04-26 08:12:03 -0700

    prints last digit function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - function to check, print and return
+ *
+ * @n: Number to pass to function
+ *
+ * Return: returns 1 or 0 or -1
+ */
+
+
+int print_last_digit(int n)
+{
+return (n % 10);
+}
