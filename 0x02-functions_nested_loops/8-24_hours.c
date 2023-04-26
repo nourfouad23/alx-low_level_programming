39546318fb9d18ec2f9146d09f6e3969a72a1084
nourfouad23 <n.fouad@xtoniq.com>
2023-04-26 15:30:45 -0700

    fixes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,9 +16,17 @@ for (hours = 0; hours <= 23; hours++)
 {
 for (minutes = 0; minutes <= 60; minutes++)
 {
-if (hours < 10 || minutes < 10)
+if (hours < 10 && minutes < 10)
 {
 printf("0%d:0%d\n", hours, minutes);
+}
+else if (hours < 10)
+{
+printf("0%d:%d\n", hours, minutes);
+}
+else if (minutes < 10)
+{
+printf("%d:0%d\n", hours, minutes);
 }
 else
 {
