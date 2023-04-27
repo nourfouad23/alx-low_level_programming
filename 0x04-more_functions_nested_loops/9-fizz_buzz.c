d400f271f1e5fb82063b319f7401689103174fba
nourfouad23 <n.fouad@xtoniq.com>
2023-04-27 16:42:33 -0700

    fixes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,9 +15,13 @@ if (num % 15 == 0)
 {
 printf("FizzBuzz ");
 }
-else if (num % 5 == 0)
+else if (num % 5 == 0 && num != 100)
 {
 printf("Buzz ");
+}
+else if (num % 5 ==0 && num == 100)
+{
+printf("Buzz");
 }
 else if (num % 3 == 0)
 {
