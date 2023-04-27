661179b8a8e3cb9b9f7361ef59b8ee3be78d8f9a
nourfouad23 <n.fouad@xtoniq.com>
2023-04-27 15:33:49 -0700

    print numbers function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - function to check, print and return
+ *
+ */
+
+void more_numbers(void)
+{
+int num;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+for (num = 0; num <= 14; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+}
+}
