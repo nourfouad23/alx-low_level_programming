ab90d2ef1110e84561b8148839c97efed5f2a68e
nourfouad23 <n.fouad@xtoniq.com>
2023-04-27 14:49:02 -0700

    multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mull - function to multiply two numbers and return
+ *
+ * @a: Number to pass to function
+ * @b: Number to pass to function
+ *
+ * Return: returns results
+ */
+
+
+int mul(int a, int b)
+{
+int result;
+
+result = a * b;
+return (result);
+}
