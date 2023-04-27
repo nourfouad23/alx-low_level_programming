2b7618595d60450080ebb0e382918fd2d521a8a7
nourfouad23 <n.fouad@xtoniq.com>
2023-04-27 15:13:58 -0700

    print most numbers function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - function to check, print and return
+ *
+ */
+
+void print_most_numbers(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+if (num != 0 && num != 2)
+printf("%d", num);
+}
+printf("\n");
+}
