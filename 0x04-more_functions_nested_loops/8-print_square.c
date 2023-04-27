1622bc4525119bba2613232cdd9c57000cb3d616
nourfouad23 <n.fouad@xtoniq.com>
2023-04-27 16:09:42 -0700

    print squares function

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - function to check, print and return
+ *
+ * @n: Number to pass
+ */
+
+void print_square(int size)
+{
+int i,j;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <=size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
