81a4c3fe23ee1a214dc621e3f5b6e2b9a965b951
nourfouad23 <n.fouad@xtoniq.com>
2023-05-02 14:40:30 -0700

    print reverse function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ * print_rev - check the code
+ *
+ *@s: passed parameter
+ *
+ *
+ */
+
+
+void print_rev(char *s)
+{
+int i;
+
+for (i = _strlen(s); i >= 0; i--)
+printf("%c", s[i]);
+
+printf("\n");
+}
