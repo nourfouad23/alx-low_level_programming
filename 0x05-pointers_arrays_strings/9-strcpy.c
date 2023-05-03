f5f6583e7500547cab56f7b08a8f98e6e178f34d
nourfouad23 <n.fouad@xtoniq.com>
2023-05-03 15:25:16 -0700

    strcpy func

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ * _strcpy - check the code
+ *
+ *@dest: passed parameter
+ *@src: passed parameter
+ *
+ * Return: returns value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; i != '\0'; i++)
+{
+dest[i] = src=[i];
+}
+
+return dest;
+}
