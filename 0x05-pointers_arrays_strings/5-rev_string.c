b33cc770e8b0f8b3c256459f8c39e655b39f4567
nourfouad23 <n.fouad@xtoniq.com>
2023-05-03 16:39:14 -0700

    fixes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include "2-strlen.c"
+#include <stdlib.h>
 
 /**
  * rev_string - check the code
@@ -13,14 +14,28 @@
 
 void rev_string(char *s)
 {
-int i;
-char temp;
+int i,j;
 
-for (i = (_strlen(s) - 1); i >= 0; i--)
+char* temp = malloc(sizeof(*temp));
+
+
+i = 0;
+j = (_strlen(s) - 1);
+
+temp[_strlen(s) - 1] = s[0];
+
+while (i < (_strlen(s) - 2))
 {
-temp = s[i];
+while ( j > 0)
+{
+temp[i] = s[j];
+j--;
+i++;
 }
-printf("%c", temp);
+}
 
-*s = temp;
+for (i = 0; i < (_strlen(s)); i++)
+{
+s[i] = temp[i];
+}
 }
