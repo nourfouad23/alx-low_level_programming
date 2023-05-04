3cf4d25047073530def1b0f0356670bf87a81f3b
nourfouad23 <n.fouad@xtoniq.com>
2023-05-04 16:13:16 -0700

    fixes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 /**
  * _strcat - check the code
@@ -15,11 +15,36 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i;
+int j, z;
 
-for (i = 0; src[i] != '\0'; i++)
+int length = strlen(dest) + strlen(src);
+
+char *temp = malloc(sizeof(*temp));
+
+int i = 0;
+
+while (i < length)
 {
-dest[(strlen(src) + 1) + i] = src[i];
+for (j = 0; dest[j] != '\0'; j++)
+{
+temp[i] = dest[j];
+i++;
+}
+for (z = 0; src[z] != '\0'; z++)
+{
+temp[i] = src[z];
+i++;
+}
+}
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+dest[i] = '\0';
+}
+
+for (i = 0; i < length; i++)
+{
+dest[i] = temp[i];
 }
 
 return (dest);
