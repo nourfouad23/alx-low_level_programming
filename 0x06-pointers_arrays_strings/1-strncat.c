3d0a427880b0d65d3b99b1a3ef35626b264496cf
nourfouad23 <n.fouad@xtoniq.com>
2023-05-05 04:09:03 -0700

    fixes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,22 +16,32 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int j, z;
+int j, z, stopper;
 
 int length = strlen(dest) + n;
+int maxLength = strlen(dest) + strlen(src);
 
 char *temp = malloc(sizeof(*temp));
 
 int i = 0;
 
-while (i < length)
+if (length > maxLength)
+{
+stopper = maxLength;
+}
+else
+{
+stopper = length;
+}
+
+while (i < stopper)
 {
 for (j = 0; dest[j] != '\0'; j++)
 {
 temp[i] = dest[j];
 i++;
 }
-for (z = 0; z < n; z++)
+for (z = 0; src[z] != '\0'; z++)
 {
 temp[i] = src[z];
 i++;
@@ -43,7 +53,7 @@ for (i = 0; dest[i] != '\0'; i++)
 dest[i] = '\0';
 }
 
-for (i = 0; i < length; i++)
+for (i = 0; i < stopper; i++)
 {
 dest[i] = temp[i];
 }
