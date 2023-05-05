53b4978c59669605b71de577981b9eff61eb9661
nourfouad23 <n.fouad@xtoniq.com>
2023-05-05 14:09:44 -0700

    reverse array func

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * reverse_array - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+
+for (i = n - 1; i >= 0; i--)
+{
+if (i != n - 1)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+}
+printf("\n");
+}
