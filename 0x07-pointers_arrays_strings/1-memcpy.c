3ddc3f1799cdb0edf5bac6ce672b4ec86ac224b4
nourfouad23 <n.fouad@xtoniq.com>
2023-05-08 16:07:51 -0700

    memcpy func

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _memcpy - check the code
+ *
+ *@dest: passed parameter
+ *@src: passed parameter
+ *@n: passed parameter
+ * 
+ * Return: returns value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+char ch;
+
+for (i = 0; i < n; i++)
+{
+if (i >= 10 && i <= 16)
+{
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+dest[i] = ch;
+}
+}
+else
+{
+dest[i] = src[i];
+}
+}
+
+
+return (dest);
+}
