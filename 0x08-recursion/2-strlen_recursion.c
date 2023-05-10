a50181fdd9b56338dd7159357a2e1f69f495f927
nourfouad23 <n.fouad@xtoniq.com>
2023-05-10 15:07:00 -0700

    strlen func

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * _strlen_recursion - check the code
+ *
+ *@s: passed parameter
+ *
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+int count = 0;
+
+if(*s)
+{
+++count;
+return (count);
+}
+
+return (_strlen_recursion(s) + 1);
+++count;
+}
