ebbb789175cd5647b3cdfc47a7aa24270ed08c02
nourfouad23 <n.fouad@xtoniq.com>
2023-05-11 16:42:52 -0700

    fixes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include "4-pow_recursion.c" 
 
 /**
  * _sqrt_recursion - check the code
@@ -14,17 +14,13 @@
 
 int _sqrt_recursion(int n)
 {
-if (n < 0)
+if (n <= 0)
 {
 return (-1);
-}
-else if (n == 0)
-{
-return (0);
 }
 else if (n == 1)
 {
 return (1);
 }
-return (n * _sqrt_recursion(n / 2));
+return (n / _sqrt_recursion(n * 0.5));
 }
