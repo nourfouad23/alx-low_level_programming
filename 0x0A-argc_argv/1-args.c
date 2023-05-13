55972c63636497ad9ebf14bb9bdebc17edacdf0e
nourfouad23 <n.fouad@xtoniq.com>
2023-05-13 11:16:29 -0700

    prints number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: number returned
+ *@argv: strings returned
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+argv = argv;
+
+printf("%d \n", argc);
+
+return (0);
+}
