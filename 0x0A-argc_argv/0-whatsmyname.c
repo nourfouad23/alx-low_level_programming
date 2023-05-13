fcbf94191cd0eaa73a31c7fac001154cbf2cf382
nourfouad23 <n.fouad@xtoniq.com>
2023-05-13 11:05:58 -0700

    fixes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *@argc: number returned
+ *@argv: strings returned
+ * Return: Always 0 (Success)
+ */
+
+
 int main (int argc, char *argv[])
 {
-int count = argc;
+argc = argc;
 
 printf("%s\n", argv[0]);
 
-return (count);
+return (0);
 }
