edc109b57e77a72aef7c866a8355f62142b108bf
nourfouad23 <n.fouad@xtoniq.com>
2023-05-13 13:31:16 -0700

    fixes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,19 +12,22 @@ int main(int argc, char *argv[])
 {
 int i, result = 1;
 
-if (argc > 2)
+if (argc > 1)
 {
 for (i = 1; i < argc; i++)
 {
 result = result * atoi(argv[i]);
 }
-printf("%d\n", result);
-return (0);
+}
+else if (argc == 1)
+{
+result = 1;
 }
 else
 {
 printf("Error");
 return (1);
 }
+printf("%d\n", result);
 return (0);
 }
