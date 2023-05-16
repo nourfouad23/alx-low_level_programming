9e07ab41306766857f5ecf440f1be9bf947b7984
nourfouad23 <n.fouad@xtoniq.com>
2023-05-16 13:42:45 -0700

    create array func

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create array
+ * @size: passed param
+ * @c: passed param
+ *
+ * Return: returns value
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *arr;
+unsigned int i;
+
+arr = malloc(size * (sizeof(c)));
+
+if (size == 0)
+{
+return NULL;
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+}
+return arr;
+}
+}
