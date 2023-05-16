1b3cd240953d01f671f6bba3db963a5ea9fa5d94
nourfouad23 <n.fouad@xtoniq.com>
2023-05-16 16:17:28 -0700

    two dimen arr

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - 2 dimen arr func
+ *@width: passed param
+ *@height: passed param
+ * Return: return value.
+ */
+
+int **alloc_grid(int width, int height)
+{
+if (width <=0 || height <= 0)
+{
+return (NULL);
+}
+else
+{
+int **array;
+
+int i;
+
+array = malloc(width * sizeof(int *));
+
+for(i = 0; i < width; i++)
+{
+array[i] = malloc(height * sizeof(int));
+}
+return (array);
+}
+}
