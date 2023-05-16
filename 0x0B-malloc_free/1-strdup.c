2963c8c17262bcfbfe9948647aeb923d365e5300
nourfouad23 <n.fouad@xtoniq.com>
2023-05-16 14:38:08 -0700

    fixes

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,26 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * strdup - copy func
+ * _strdup - copy func
  *@str: passed param
  * Return: return value.
  */
 
 char *_strdup(char *str)
 {
-char *newstr = malloc(sizeof(*str));
+char *newstr = (char *) malloc(strlen(str) * sizeof(char));
 
-int i;
+long unsigned int i;
 
-if(str == NULL)
+if (str == NULL)
 {
 return (NULL);
 }
 else
 {
-for (i = 0; str[i] != '\0'; i++)
+for (i = 0; i <= strlen(str); i++)
 {
 newstr[i] = str[i];
 }
