c8d1aa16a673b52fd17fd40498d343a3a3c321b7
nourfouad23 <n.fouad@xtoniq.com>
2023-05-17 12:33:24 -0700

    fixes

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code
+ *
+ *@c: passed parameter
+ *
+ * Return: return value
+ */
+
+
+int _isdigit(int c)
+{
+return (0);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check the code
+ *
+ *@str: passed parameter
+ *
+ *
+ */
+
+
+void _puts(char *str)
+{
+printf("%s\n", str);
+}
