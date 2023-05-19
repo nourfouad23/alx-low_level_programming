820d929cd8b8426fd5b6c859ccd218226df5f63f
nourfouad23 <n.fouad@xtoniq.com>
2023-05-19 15:45:30 -0700

    fixes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,20 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *newstr;
-unsigned i, j ,z;
+unsigned int i, j, z;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
 
 if (n >= sizeof(s2))
 {
@@ -22,7 +35,7 @@ newstr = (char *) malloc(((strlen(s1) + strlen(s2)) * sizeof(char)) + 1);
 }
 else if (n < sizeof(s2))
 {
-newstr = (char *) malloc(((strlen(s1) + n * sizeof(char)) + 1));
+newstr = (char *) malloc((((strlen(s1) + n + 1)* sizeof(char))));
 }
 else
 {
