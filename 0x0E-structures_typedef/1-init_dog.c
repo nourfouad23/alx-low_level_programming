7cd64cb0c603ca476d544b46b321f454708e5e97
nourfouad23 <n.fouad@xtoniq.com>
2023-05-22 13:55:02 -0700

    init dog func

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - check the code
+ *@d: passed param
+ *@name: passed param
+ *@age: passed param
+ *@owner: passed param
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
