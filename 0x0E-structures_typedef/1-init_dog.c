822cd4b950d6d564c137eb19a2565ee0efdf1944
nourfouad23 <n.fouad@xtoniq.com>
2023-05-22 13:59:09 -0700

    fixes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,13 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog dogStr;
-
-d = &dogStr;
+if (d == NULL)
+	return(1);
 
 d -> name = name;
-
 d -> age = age;
-
 d -> owner = owner;
 }
