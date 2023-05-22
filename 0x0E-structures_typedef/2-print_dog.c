ec951c6585c1e8d50c7691fc652cc33caca4df53
nourfouad23 <n.fouad@xtoniq.com>
2023-05-22 15:17:01 -0700

    fixes and print dog func

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "dog.h"
+#include "main.h"
+
+/**
+ * print_dog - initialize function for struct dog
+ * @d: pointer to dog struct
+ * 
+ * Return: return pointer to struct
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+if (d->age == NULL)
+{
+printf("Age: (nil)\n");
+}
+else
+{
+printf("Age: %f\n", d->age);
+}
+if(d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
