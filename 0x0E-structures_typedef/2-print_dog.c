ecf7e0366818ca8d55229be658c3c5c53410dd8d
nourfouad23 <n.fouad@xtoniq.com>
2023-05-22 15:37:51 -0700

    fixes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,14 +23,9 @@ else
 {
 printf("Name: %s\n", d->name);
 }
-if (d->age == NULL)
-{
-printf("Age: (nil)\n");
-}
-else
-{
+
 printf("Age: %f\n", d->age);
-}
+
 if(d->owner == NULL)
 {
 printf("Owner: (nil)\n");
