3e91e918f71ce5178b25c98357fd759bfa1019bc
nourfouad23 <n.fouad@xtoniq.com>
2023-05-23 15:10:50 -0700

    compiled file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - program to print the file name
+ * Description: print file name from which file is compiled from
+ * Return: always a success (0)
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
