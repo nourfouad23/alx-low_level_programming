a8cff441eada7b75fbff32023fa5d51f6980aaba
nourfouad23 <n.fouad@xtoniq.com>
2023-06-02 10:28:52 -0700

    fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,12 +14,15 @@ size_t print_list(const list_t *h)
 int count = 0;
 
 while (h != NULL) {
-printf("%d ", h->len);
+if (h->str == NULL)
+printf("[0] (nil)\n");
+
+
+printf("[%d] %s\n", h->len, h->str);
 h = h->next;
 count++;
 }
 
-printf("\n");
 
 return (count);
 }
