c1232f2de909f1e42e9a1d30e5218a75fb4d4f56
nourfouad23 <n.fouad@xtoniq.com>
2023-06-02 12:07:48 -0700

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - check the code
+ *
+ *@head: passed param
+ *@str: passeed param
+ *
+ * Return: return number of elements.
+ *
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *newNode;
+
+newNode = malloc(sizeof(list_t));
+
+if (newNode == NULL)
+return (NULL);
+
+newNode->str = strdup(str);
+
+if (*head == NULL)
+{
+*head = newNode;
+
+return (*head);
+}
+newNode->next = *head;
+*head = newNode;
+
+return (*head);
+}
