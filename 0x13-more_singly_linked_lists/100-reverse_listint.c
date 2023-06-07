a2bb98e7ffbd130c8184dba88f75e24a674aeb27
nourfouad23 <n.fouad@xtoniq.com>
2023-06-07 16:20:09 -0700

    fixes

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -20,8 +20,8 @@ listint_t *next_node = NULL;
 
 while (*head != NULL)
 {
-next_node = (*head)->next_node;
-(*head)->next_node = prev_node;
+next_node = (*head)->next;
+(*head)->next = prev_node;
 prev_node = *head;
 *head = next_node;
 }
