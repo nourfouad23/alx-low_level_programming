6185de8dd83b42d36914099b8ac3942583cc5d42
nour.fouad <n.fouad@xtoniq.com>
2023-07-07 13:51:32 -0700

    static lib

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code
+ *
+ *@c: passed parameter
+ *
+ * Return: return value
+ */
+
+
+int _isupper(int c)
+{
+return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include<string.h>
+
+/**
+ * _strlen - check the code
+ *
+ *@s: passed parameter
+ *
+ *Return: returns value
+ *
+ */
+
+
+int _strlen(char *s)
+{
+int count = 0;
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+count++;
+}
+return (count);
+}
