0216bc8c89b212e1c0f28cf687cde42f152d8136
nour.fouad <n.fouad@xtoniq.com>
2023-07-07 14:59:52 -0700

    power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * _pow_recursion - check the code
+ *
+ *@x: passed parameter
+ *@y: passed parameter
+ *
+ * Return: return value
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
