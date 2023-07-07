b4c7b038e2e65f5c98c96b444eb74e1cc5bfec1f
nour.fouad <n.fouad@xtoniq.com>
2023-07-07 14:38:29 -0700

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * _print_rev_recursion - check the code
+ *
+ *@s: passed parameter
+ *
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+printf("%c", *s);
+}
+}
