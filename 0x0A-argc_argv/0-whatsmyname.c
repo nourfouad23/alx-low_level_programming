82267f6a7817dfa6a7d6c286abd4f62474b1b675
nour.fouad <n.fouad@xtoniq.com>
2023-07-07 14:24:57 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: number returned
+ *@argv: strings returned
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+argc = argc;
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
