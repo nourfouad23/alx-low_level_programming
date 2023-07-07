a805b742d6b90179f460d0b3a49df24a5f5311f5
nour.fouad <n.fouad@xtoniq.com>
2023-07-07 14:33:44 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *@argc: number returned
+ *@argv: strings returned
+ *Return: returns value
+ */
+
+int main(int argc, char *argv[])
+{
+int i, result;
+
+result = 1;
+
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+result = result * atoi(argv[i]);
+}
+}
+else if (argc == 0)
+{
+result = 1;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", result);
+return (0);
+}
