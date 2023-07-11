6e988a2a11f59ec608b0b52a5bbc1856cb5a213e
nour.fouad <n.fouad@xtoniq.com>
2023-07-11 14:50:14 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_grid - free func
+ *@grid: passed param
+ *@height: passed param
+ * Return: return nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+if (!(height <= 0))
+{
+int i;
+
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
+}
