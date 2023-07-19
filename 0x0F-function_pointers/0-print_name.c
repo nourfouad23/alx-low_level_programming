b4798cccdc3fa76276468f2b7eaf4d94caed33a5
nour.fouad <n.fouad@xtoniq.com>
2023-07-19 14:33:11 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - copy func
+ *@f: passed param
+ *@name: passed param
+ * Return: return nothing.
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+void (* ptr)(char*);
+
+if (name == NULL || f == NULL)
+return;
+
+
+ptr = f;
+ptr(name);
+}
