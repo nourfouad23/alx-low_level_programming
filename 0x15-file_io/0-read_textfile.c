88c2d9f3df1f106d8712dfda9505e362381020eb
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2023-08-07 23:49:02 +0300

    fixes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * read_textfile - reads a text file and prints it to standard output
  * @filename: relative or absolute path of the file
