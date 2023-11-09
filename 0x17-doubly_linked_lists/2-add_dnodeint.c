58c9cd8edacc9d4d93cd0cd42041765a6bc3aa67
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2023-11-10 00:14:12 +0200

    adds new node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds new node
+ * @head: head pointer
+ * @n: param
+ * Return: new node
+ **/
+ 
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+	*head = new;
+	return (new);
+	}
+	(*head)->prev = new;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
