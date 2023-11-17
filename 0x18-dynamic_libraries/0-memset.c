fea9d8b75c950e36386548d01404a7db74050043
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2023-11-18 00:33:13 +0200

    dynamic lib

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memset - check the code
+ *
+ *@b: passed parameter
+ *@s: passed parameter
+ *@n: passed parameter
+ * Return: return value
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+return (0);
+}
